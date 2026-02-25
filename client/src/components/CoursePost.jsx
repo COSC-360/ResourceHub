639bd59d6fdd3d2a7aff434bfb6a128269a4c40c
zaladmander <159731834+zaladmander@users.noreply.github.com>
2026-02-25 10:44:25 -0800

    some more changes to make it look less like shiz

--- client/src/components/CoursePost.jsx
+++ client/src/components/CoursePost.jsx
@@ -1,13 +1,23 @@
 function CoursePost({ course }) {
     let courseName = course.name + " " + course.code;
     let courseDescription = course.description;
+    let courseImage = course.image;
     let numberOfStudents = course.numberOfStudents;
 
     return (
-        <div>
-            <h1>{courseName}</h1>
-            <p>{courseDescription}</p>
-            <p>Number of Students: {numberOfStudents}</p>
-        </div>
+        <article className="course-post">
+            <header>
+                <div>
+                    <h3>{courseName}</h3>
+                    <p>{courseDescription}</p>
+                </div>
+                <button>⋮</button>
+            </header>
+            
+            <img src={courseImage} alt={`${courseName} course image`} />
+            <img /><p>Number of Students: {numberOfStudents}</p> // img icon is three people thingy
+        </article>
     )
 }
+
+export default CoursePost;
