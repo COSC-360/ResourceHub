a17d175ac398315477625afd043add5bc20f83e3
zaladmander <159731834+zaladmander@users.noreply.github.com>
2026-02-25 11:06:51 -0800

    probably finished course post component

--- client/src/components/CoursePost.jsx
+++ client/src/components/CoursePost.jsx
@@ -1,3 +1,5 @@
+import './css/CoursePost.css';
+
 function CoursePost({ course }) {
     let courseName = course.name + " " + course.code;
     let courseDescription = course.description;
@@ -7,15 +9,21 @@ function CoursePost({ course }) {
     return (
         <article className="course-post">
             <header>
-                <div>
+                <div className="course-info">
                     <h3>{courseName}</h3>
                     <p>{courseDescription}</p>
                 </div>
-                <button>⋮</button>
+                <button className="menu-button"><img src="/src/assets/menu-icon.svg" alt="Menu icon" /></button>
             </header>
             
-            <img src={courseImage} alt={`${courseName} course image`} />
-            <img /><p>Number of Students: {numberOfStudents}</p> // img icon is three people thingy
+            <img className="course-image" src={courseImage} alt={`${courseName} course image`} />
+            
+            <footer>
+                <p>
+                    <img className="student-count-icon" src="/src/assets/student-count-icon.svg" alt="Student count icon" />
+                    {numberOfStudents}
+                </p> 
+            </footer>
         </article>
     )
 }
