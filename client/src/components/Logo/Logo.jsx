fefb907162f447b33928f4323f1a1c582a5c8501
zaladmander <159731834+zaladmander@users.noreply.github.com>
2026-03-25 10:27:16 -0700

    fixed logo css and added other fallback fonts

--- client/src/components/Logo/Logo.jsx
+++ client/src/components/Logo/Logo.jsx
@@ -2,7 +2,7 @@ export function Logo() {
     return (
         <>
             {/*This will be replaced with a logo once one is made*/}
-            <a href="" style={{ color: 'white', fontFamily: 'fantasy', fontSize: '40px', textDecoration: 'none', justifyContent: 'center', padding:'0px 0px 0px 40px'}}>
+            <a class="logo" href="">
                 Resource Hub
             </a>
         </>
