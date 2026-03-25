26770a9230eb477de46b4ec4db637dfc7017d934
zaladmander <159731834+zaladmander@users.noreply.github.com>
2026-03-25 11:17:17 -0700

    fix reset css priority

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { AuthProvider } from "./AuthContext.jsx";
+import './reset.css'
 import './index.css'
 import App from './App.jsx'
-import './reset.css'
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
