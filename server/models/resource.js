36577e553fd434aff130dfa2f438c4c0b14a3ee7
zaladmander <159731834+zaladmander@users.noreply.github.com>
2026-03-28 21:50:17 -0700

    updated resource model

--- server/models/resource.js
+++ server/models/resource.js
@@ -1,7 +1,14 @@
+/*
+represents a resource shared in a course, such as a document, link, 
+or video. It includes properties like name, description, URL, category, 
+and metadata such as likes, dislikes, file count, and download count. 
 
+The class also provides methods to like or dislike the resource.
+*/
 export class Resource {
     constructor(data) {
         this.id = data.id;
+        this.courseId = data.courseId;
         this.name = data.name;
         this.description = data.description;
         this.url = data.url;
