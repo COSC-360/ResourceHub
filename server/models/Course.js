0abead4d55427f8b1541b490cabc865960986790
zaladmander <159731834+zaladmander@users.noreply.github.com>
2026-03-31 20:54:14 -0700

    documentation

--- server/models/Course.js
+++ server/models/Course.js
@@ -12,9 +12,9 @@ Represents a course in the system, containing properties such as:
 
 const CourseSchema = new mongoose.Schema({
     name: { type: String, required: true },
-    code: { type: String, required: true, unique: true },
+    code: { type: String, required: true, unique: true }, // can't have two COSC221 courses, that would cause the end of the world
     description: { type: String, default: "" },
-    image: { type: String, default: null },
+    image: { type: String, default: null }, // since multer will store the image as a URL or file path
     memberCount: { type: Number, default: 0 },
     postCount: { type: Number, default: 0 },
 }, { timestamps: true }); // Automatically adds createdAt and updatedAt fields
