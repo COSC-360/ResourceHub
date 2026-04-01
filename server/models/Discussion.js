73661527aefe378e36d0bccf41496df6c61a8aa3
zaladmander <159731834+zaladmander@users.noreply.github.com>
2026-03-31 22:10:02 -0700

    documentation

--- server/models/Discussion.js
+++ server/models/Discussion.js
@@ -1,5 +1,20 @@
 import mongoose from "mongoose";
 
+/*
+Represents a discussion post within a course, allowing users to create threads and reply to them.
+It includes properties like:
+    courseId: The ID of the course the discussion belongs to
+    authorId: The ID of the user who created the discussion
+    image: An optional image URL associated with the discussion
+    title: An optional title for the discussion
+    content: The main content of the discussion post
+    edited: A boolean indicating whether the post has been edited
+    upvotes: The number of upvotes the discussion has received, cached. votes are stored in the Vote collection and this field is updated accordingly.
+    downvotes: The number of downvotes the discussion has received, cached. votes are stored in the Vote collection and this field is updated accordingly.
+    replies: The number of replies to this discussion, cached. This field is updated whenever a new reply is added.
+    parentId: If this discussion is a reply, this field references the parent discussion's ID. Otherwise, it is null.
+*/
+
 const DiscussionSchema = new mongoose.Schema({
   courseId: { type: mongoose.Schema.Types.ObjectId, ref: "Course", required: true },
   authorId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
