71c198ad8e41397df40412f3a881f65fd674da25
zaladmander <159731834+zaladmander@users.noreply.github.com>
2026-03-31 22:38:22 -0700

    typo

--- server/services/courseService.js
+++ server/services/courseService.js
@@ -1,4 +1,4 @@
-import { Course } from '../models/Course.js';
+import { Course } from '../models/course.js';
 import * as courseRepository from '../repositories/courseRepository.js';
 
 export function create(course) {
