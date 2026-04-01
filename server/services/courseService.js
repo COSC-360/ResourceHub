c6c499728ee6b5f1277dcd7e13467454c675f67d
zaladmander <159731834+zaladmander@users.noreply.github.com>
2026-04-01 00:32:56 -0700

    just cleaning up ig

--- server/services/courseService.js
+++ server/services/courseService.js
@@ -1,15 +1,19 @@
 import { Course } from '../models/course.js';
-import * as courseRepository from '../repositories/courseRepository.js';
-
-export function create(course) {
-    const newCourse = Course.create(course.name, course.code, course.description, course.image);
-    return courseRepository.save(newCourse.toJSON());
-}
+import { courseRepository } from '../repositories/courseRepository.js';
 
 export function getById(id) {
     return courseRepository.findById(id);
 }
 
+export function create(course) {
+    return courseRepository.save({
+        name: course.name,
+        code: course.code,
+        description: course.description,
+        image: course.image,
+    });
+}
+
 export function getAll() {
-  return courseRepository.findAll();
+    return courseRepository.findAll();
 }
