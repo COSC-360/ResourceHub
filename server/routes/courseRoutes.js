910d62b4c1948e81bf9e1ec7b0c067f176e48f68
zaladmander <159731834+zaladmander@users.noreply.github.com>
2026-04-07 10:03:23 -0700

    todos

--- server/routes/courseRoutes.js
+++ server/routes/courseRoutes.js
@@ -7,12 +7,15 @@ export const courseRoutes = Router();
 
 courseRoutes.get("/", courseController.getAll); // get all courses
 courseRoutes.get("/:id", courseController.getById); // get one course by id
+
+// TODO: when course is created, also need to create a course membership for the creator (probably want to do this in the service layer)
 courseRoutes.post("/create", verifyAccessToken, courseController.create); // create a new course
 
+// TODO: add auth middleware to these routes, also need to check if user is an instructor for the course when updating or deleting
 courseRoutes.patch("/:id/update", verifyAccessToken, courseController.update); // update a course by id
 courseRoutes.patch("/:id/updateimage", verifyAccessToken, upload.single("image"), courseController.updateImage); // update a course's image by id
 
-// TODO: delete a course by id, probably want auth 
+// TODO: delete a course by id, probably want auth also check membership and role (only allow instructors to delete courses)
 courseRoutes.delete("/:id", verifyAccessToken, courseController.deleteCourse); // delete a course by id
 
 // additional routes for discussions, resources, and members specific pages
